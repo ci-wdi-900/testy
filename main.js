85fba0fc65aa8c6e54033c60a557f190849f974b
Colin Jaffe <colin.jaffe@gmail.com>
2020-03-12 12:46:30 -0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,21 @@
+/******************
+ * YOUR CODE HERE *
+ ******************/
+
+
+
+
+
+
+/*********************************
+ * OUR CODE BELOW; DO NOT TOUCH! *
+ *********************************/
+
+if (typeof exampleFunction === 'undefined') {
+  exampleFunction = undefined;
+}
+
+
+module.exports = {
+  exampleFunction,
+}
